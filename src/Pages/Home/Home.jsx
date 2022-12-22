c5bd3fc01121f980cfe714a50b624f8d6662ed28
Md Al Amin <fpalamin6@gmail.com>
2022-12-22 23:29:03 +0600

    last commited

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,98 @@
 import React from 'react';
+import { Cell, Customized, Pie, PieChart, PolarAngleAxis, PolarRadiusAxis } from 'recharts';
+// import { PieChart } from 'react-minimal-pie-chart';
 
 const Home = () => {
+    const data01 = [
+        {
+            "name": "Group A",
+            "value": 400
+        },
+        {
+            "name": "Group B",
+            "value": 300
+        },
+        {
+            "name": "Group C",
+            "value": 300
+        },
+        {
+            "name": "Group D",
+            "value": 200
+        },
+        {
+            "name": "Group E",
+            "value": 278
+        },
+        {
+            "name": "Group F",
+            "value": 189
+        }
+    ];
+    const data02 = [
+        {
+            "name": "Group A",
+            "value": 2400
+        },
+        {
+            "name": "Group B",
+            "value": 4567
+        },
+        {
+            "name": "Group C",
+            "value": 1398
+        },
+        {
+            "name": "Group D",
+            "value": 9800
+        },
+        {
+            "name": "Group E",
+            "value": 3908
+        },
+        {
+            "name": "Group F",
+            "value": 4800
+        }
+    ];
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+    
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
     return (
-        <div>
-            
+        <div >
+            <PieChart width={930} height={550}>
+                <Pie data={data01} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+               
+                <Pie
+            data={data01}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            innerRadius={80}
+            outerRadius={200}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data01.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+            </PieChart>
+           
         </div>
     );
 };
